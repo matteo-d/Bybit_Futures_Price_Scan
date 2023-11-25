c16340f3a6070d8217b575ec6609ec06631a384c
matteo-d <matteodrouin33@gmail.com>
2023-11-25 19:02:24 +0000

    Save : Working on updating and adding charts data

--- scan.py
+++ scan.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 # Load my data base that contains price levels
 with open("data.json") as f:
-        levels_data = json.load(f)
+    levels_data = json.load(f)
 
 # Bybit
 bybit = ccxt.bybit()
@@ -15,14 +15,22 @@ base_url = "https://api.bybit.com/v2"
 headers = {"Content-Type": "application/json", "api-key": ""}
 
 # List of undesired coins
-undesired_tickers = ["WSMUSDT","WAXPUSDT","TOMIUSDT","STRAXUSDT","STEEMUSDT","RIFUSDT","POWRUSDT","POLYXUSDT","ORBSUSDT","NTRNUSDT","GASUSDT","GASUSDT","BONDUSDT","BIGTIMEUSDT","10000STARLUSDT","STPTUSDT","VRAUSDT","MNTUSDT","ETHUSDH24","BTCUSDH24","LOOMUSDT","MULTIUSDT","FRONTUSDT","HNTUSDT","XVSUSDT","PERPUSDT","PROMUSDT","NMRUSDT","TONUSDT","OGUSDT","HIFIUSDT","YFIIUSDT","BADGERUSDT","SEIUSDT","CYBERUSDT","RDNTUSDT","QNTUSDT","BTCUSDZ23","COMBOUSDT","UMAUSDT","KEYUSDT","RADUSDT","LEVERUSDT","AMBUSDT","10000LADYSUSDT","TUSDT","TRUUSDT","DOTUSD","GFTUSDT","NKNUSDT", "AGIXUSDT","USDCUSDT","COREUSDT","EOSUSD","FETUSDT", "ATOMUSD","ADAUSD","BITUSD","BTCUSD","BTCUSDH23","BTCUSDM23","BTCUSDZ22","C98USD","ETHUSD","ETHUSDH23","ETHUSDM23","ETHUSDZ23","ETHWUSDT","LTCUSD","MANAUSD","USDCUSDT","BUSDUSDT","XRPUSD","SSVUSDT","RPLUSDT","LQTYUSDT","GPTUSDT", "FLRUSDT","ETHUSDU23","HFTUSDT","BTCUSDU23","IDUSDT","RLCUSDT","ORDIUSDT","KEYUSDT"]
-webbrowser.open("https://www.youtube.com/watch?v=kse87ocS0Uo", new=0)
+undesired_tickers = [
+    "C98USD", "LTCUSD", "ADAUSD", "BITUSD", "USDCUSDT",
+    "BUSDUSDT", "DOTUSD", "ETHUSDU23", "BTCUSDU23", "XRPUSD",
+    "MANAUSD", "ETHUSD", "ETHUSDH23", "ETHUSDM23", "ETHUSDZ23",
+    "ETHWUSDT", "BTCUSD", "BTCUSDH23", "BTCUSDM23", "BTCUSDZ22",
+    "ATOMUSD", "TUSDT", "USDCUSDT", "BTCUSDZ23", "BTCUSDH24", "ETHUSDH24"
+]
+
 # Get the current price for x ticker
+
+
 def get_current_price(ticker):
     # Connect to bybit
     response = requests.get(base_url + "/public/tickers", headers=headers)
     if response.status_code != 200:
-            print("failed with status code :", response.status_code)
+        print("failed with status code :", response.status_code)
     else:
         data = response.json()
         for response_data in data['result']:
@@ -30,7 +38,8 @@ def get_current_price(ticker):
                 current_price = float(response_data['last_price'])
                 return current_price
 
-def getListOfTickersCloseToLevel() :
+
+def getListOfTickersCloseToLevel():
     # Connect to bybit
     response = requests.get(base_url + "/public/tickers", headers=headers)
     if response.status_code != 200:
@@ -38,31 +47,29 @@ def getListOfTickersCloseToLevel() :
     else:
         data = response.json()
         result_array = []
-        # Check if the coin tracked correspond to the coins levels in the database 
+        # Check if the coin tracked correspond to the coins levels in the database
         c = 0
         for ticker in data['result']:
             if ticker['symbol'] not in undesired_tickers:
                 if ticker['symbol'] != levels_data[c]['ticker']:
-                    print('ERROR AT' , ticker['symbol'] , 'vs', levels_data[c]['ticker'], c )
+                    print('ERROR AT', ticker['symbol'],
+                          'vs', levels_data[c]['ticker'], c)
                     exit()
                     return
                 c += 1
-      
-        for ticker in data["result"] :
-            if ticker['symbol'] not in undesired_tickers :
+
+        for ticker in data["result"]:
+            if ticker['symbol'] not in undesired_tickers:
                 for x in levels_data:
                     if ticker['symbol'] == x['ticker']:
                         for inner_list in x['levels']:
                             price_of_interest = float(inner_list[0])
-                            grade = int(inner_list[1])
                             current_price = float(ticker['last_price'])
                             percentage = float(x['close'])
-                            # grade == 0 means mid range, the zone of interest of a mid range is 2 times smaller than the normal price_of_interest
-                            if grade == 0 and price_of_interest - price_of_interest * (percentage/2) <= current_price <= price_of_interest + price_of_interest * (percentage/2) :
-                                result_array.append(ticker['symbol'])
-                            if grade != 0 and price_of_interest - price_of_interest * percentage <= current_price <= price_of_interest + price_of_interest * percentage :
+                            if price_of_interest - price_of_interest * percentage <= current_price <= price_of_interest + price_of_interest * percentage:
                                 result_array.append(ticker['symbol'])
         return result_array
+
 
 def filterResultArray(movers):
     print("Movers", movers)
@@ -75,14 +82,17 @@ def filterResultArray(movers):
                 print("CHART : ", y["web"])
                 print("")
                 webbrowser.open(y["web"], new=0)
+                time.sleep(9)
                 already_visited_chart.append(y["ticker"])
+
+
 # Variables to know if a chart already has been visited inside the loop
 counter = 0
 added = []
 deleted = []
 already_visited_chart = []
 
-# Infinite loop 
+# Infinite loop
 while True:
     # Get a price snapshot of all the assets listed on bybit futures
     first_run = getListOfTickersCloseToLevel()
@@ -90,7 +100,7 @@ while True:
     second_run = getListOfTickersCloseToLevel()
 
     # Reset Already visited charts after x loops
-    if counter == 4:
+    if counter == 3:
         added = []
         deleted = []
         already_visited_chart = []
